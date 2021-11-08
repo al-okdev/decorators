085ea4b239988527a502bca77d4a6bc9d4874813
Alexander <alokdev@mail.ru>
2021-11-08 20:15:06 +0300

    dz_1

---
+++ main.py
@@ -0,0 +1,30 @@
+import datetime
+import time
+
+
+def decor(old_fuction):
+    def new_function(*args, **kwars):
+        result = old_fuction(*args, **kwars)
+
+        with open('result.txt', "a+", encoding="utf-8") as result_file:
+            result_file.write(f'Дата и время: {datetime.datetime.now()}' + '\n')
+            result_file.write(f'Функция: {old_fuction.__name__}' + '\n')
+            result_file.write(f'Аргументы: {args} {kwars}' + '\n')
+            result_file.write(f'Возвращаемый результат: {result}' + '\n')
+            result_file.write('\n')
+
+        return result
+
+    return new_function
+
+
+
+@decor
+def my_function(a, b):
+    return a + b
+
+print('Запуск')
+my_function(3, 4)
+time.sleep(2)
+my_function(6, 4)
+print('Остановка программы')
